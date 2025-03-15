40229b720b352194dda9c05cc5b4b0f4fad2acb5
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-03-15 03:16:04 -0700

    python version update

--- post/models.py
+++ post/models.py
@@ -12,6 +12,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return self.content
     
