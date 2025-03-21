3a76e1be7e0efbd2ae1bc07ba3fb3005bf85af8b
SARWAR JAHIN <sarwarjahinpersonal@gmail.com>
2025-03-21 03:33:03 -0700

    email activated system added successfully

--- pixello/settings.py
+++ pixello/settings.py
@@ -160,6 +160,9 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': True,
     'SERIALIZERS': {
         'user_create': 'user.serializers.UserCreateSerializer',
         'current_user': 'user.serializers.UserSerializer'
@@ -188,3 +191,10 @@ cloudinary.config(
 
 # Media storage settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
